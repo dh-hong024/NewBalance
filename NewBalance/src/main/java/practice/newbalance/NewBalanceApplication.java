ab2819f8f0be93793f117eb8affba119c5e78961
홍덕환 <dh.hong024@gmail.com>
2024-04-17 18:18:21 +0900

    #3 - 개발환경 만들기
    새로운 프로젝트 생성

---
+++ NewBalance/src/main/java/practice/newbalance/NewBalanceApplication.java
@@ -0,0 +1,13 @@
+package practice.newbalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewBalanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewBalanceApplication.class, args);
+    }
+
+}
---
+++ NewBalance/src/test/java/practice/newbalance/NewBalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.newbalance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewBalanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
