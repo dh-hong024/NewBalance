9941cb1399be4c1cd9b442dbea8f39f72c6fb821
junhwa <prkr998@gmail.com>
2024-05-08 19:08:43 +0900

    시큐리티 커밋

--- NewBalance/src/main/java/practice/newbalance/config/handler/MemberAuthSuccessHandler.java
+++ NewBalance/src/main/java/practice/newbalance/config/handler/MemberAuthSuccessHandler.java
@@ -3,6 +3,7 @@ package practice.newbalance.config.handler;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
@@ -12,6 +13,9 @@ public class MemberAuthSuccessHandler extends SimpleUrlAuthenticationSuccessHand
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         //로그인 성공시 이동할 페이지
+        HttpSession session = request.getSession();
+        session.setAttribute("principal", authentication.getName());
+
         setDefaultTargetUrl("/");
         //로그인 성공시 이동할 페이지로 이동
         super.onAuthenticationSuccess(request, response, authentication);
