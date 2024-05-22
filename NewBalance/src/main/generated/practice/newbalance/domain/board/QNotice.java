3577943c9923b85c7499a6f45363431abf0b2c57
junhwa <prkr998@gmail.com>
2024-05-22 14:56:13 +0900

    #10 공지사항 구현 더보기 버튼 페이징 기능, 조회수 QEntity 추가 커밋

---
+++ NewBalance/src/main/generated/practice/newbalance/domain/board/QNotice.java
@@ -0,0 +1,57 @@
+package practice.newbalance.domain.board;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QNotice is a Querydsl query type for Notice
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QNotice extends EntityPathBase<Notice> {
+
+    private static final long serialVersionUID = 1273910091L;
+
+    public static final QNotice notice = new QNotice("notice");
+
+    public final practice.newbalance.domain.QModifierEntity _super = new practice.newbalance.domain.QModifierEntity(this);
+
+    //inherited
+    public final StringPath createdBy = _super.createdBy;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdDate = _super.createdDate;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    //inherited
+    public final StringPath modifiedBy = _super.modifiedBy;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> modifiedDate = _super.modifiedDate;
+
+    public final StringPath noticeContent = createString("noticeContent");
+
+    public final NumberPath<Integer> noticeCount = createNumber("noticeCount", Integer.class);
+
+    public final StringPath noticeTitle = createString("noticeTitle");
+
+    public QNotice(String variable) {
+        super(Notice.class, forVariable(variable));
+    }
+
+    public QNotice(Path<? extends Notice> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QNotice(PathMetadata metadata) {
+        super(Notice.class, metadata);
+    }
+
+}
+
