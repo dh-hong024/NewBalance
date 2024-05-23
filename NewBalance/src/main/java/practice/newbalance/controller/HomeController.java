755d8d2eae78db42f8f19f9fd3245e0f9dc2cc61
홍덕환 <dh.hong024@gmail.com>
2024-05-23 18:30:52 +0900

    #12 - 마이페이지 페이지 화면 추가

--- NewBalance/src/main/java/practice/newbalance/controller/HomeController.java
+++ NewBalance/src/main/java/practice/newbalance/controller/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String home(){
         return "home";
     }
+
+    @GetMapping("/my")
+    public String myPage(){
+        return "member/user/myPage";
+    }
 }
