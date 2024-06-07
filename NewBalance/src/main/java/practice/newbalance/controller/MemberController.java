dd414764093aacae8d7996560f7f5df4b491f1eb
홍덕환 <dh.hong024@gmail.com>
2024-06-07 18:07:53 +0900

    #20 - 마이페이지 배송지 관리 CRUD 구현

--- NewBalance/src/main/java/practice/newbalance/controller/MemberController.java
+++ NewBalance/src/main/java/practice/newbalance/controller/MemberController.java
@@ -3,17 +3,23 @@ package practice.newbalance.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
+import practice.newbalance.config.security.CustomUserDetail;
+import practice.newbalance.domain.member.DeliveryAddress;
+import practice.newbalance.dto.member.DeliveryAddressDto;
 import practice.newbalance.dto.member.MemberDto;
 import practice.newbalance.service.MemberService;
 import practice.newbalance.web.validator.CheckEmailValidator;
 import practice.newbalance.web.validator.CheckUserIdValidator;
 
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -95,6 +101,78 @@ public class MemberController {
         return memberService.inquiryResetPw(userId, name, phoneNumber);
     }
 
+    /**
+     * 마이페이지 이동
+     * @return
+     */
+    @GetMapping("/my")
+    public String myPage(){
+        return "member/user/myPage";
+    }
 
+    @GetMapping("/my/delivery-addr")
+    public String deliveryAddressHome(
+            Model model,
+            @AuthenticationPrincipal CustomUserDetail customUserDetail){
+        List<DeliveryAddress> addressList = memberService.getAddress(customUserDetail.getMember().getId());
+        model.addAttribute("myContents", addressList);
+        return "member/user/myPage-deliveryAddress";
+    }
 
+    /**
+     * 배송지 추가 폼 이동
+     * @return
+     */
+    @GetMapping("/my/delivery-addr-form")
+    public String deliveryAddressAddForm(){
+        return "member/user/myPage-deliveryAddForm";
+    }
+
+    /**
+     * 배송지 수정 폼 이동
+     * @param addrId
+     * @param model
+     * @return
+     */
+    @GetMapping("/my/delivery-addr/{addrId}")
+    public String deliveryUpdateForm(
+            @PathVariable("addrId") Long addrId,
+            Model model
+    ){
+        DeliveryAddress detailAddress = memberService.getDetailAddress(addrId);
+        model.addAttribute("deliveryAddr", detailAddress.toDTO());
+        return "member/user/myPage-deliveryUpdateForm";
+    }
+
+    /**
+     * 배송지 주소 추가
+     * @param customUserDetail
+     * @param dto
+     * @return
+     */
+    @PostMapping("/my/delivery-addr")
+    public ResponseEntity<String> addDeliveryAddress(
+            @AuthenticationPrincipal CustomUserDetail customUserDetail,
+            DeliveryAddressDto dto
+    ){
+        return memberService.saveAddress(customUserDetail.getMember().getId(), dto);
+    }
+
+    @PutMapping("/my/delivery-addr/{addrId}")
+    public ResponseEntity<String> updateDeliveryAddress(
+            @PathVariable("addrId") long addrId,
+            DeliveryAddressDto dto
+    ){
+        return memberService.updateAddress(addrId, dto);
+    }
+
+    /**
+     * 배송지 주소 삭제
+     * @param addrId
+     * @return
+     */
+    @DeleteMapping("/my/delivery-addr/{addrId}")
+    public ResponseEntity<String> DeleteDeliveryAddr(@PathVariable("addrId") long addrId){
+        return memberService.deleteAddress(addrId);
+    }
 }
---
+++ NewBalance/src/main/java/practice/newbalance/domain/member/DeliveryAddress.java
@@ -0,0 +1,48 @@
+package practice.newbalance.domain.member;
+
+import jakarta.persistence.*;
+import lombok.*;
+import practice.newbalance.dto.member.DeliveryAddressDto;
+
+@Entity
+@Getter @Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "DeliveryAddr")
+public class DeliveryAddress {
+    @Id @GeneratedValue
+    private Long id;
+
+    @JoinColumn(name = "member_id")
+    private Long memberId;
+
+    private String recipient;
+
+    private String destination;
+
+    private String recipientNumber;
+
+    private String otherNumber;
+
+    private String zipCode;
+
+    private String address;
+
+    private String detailAddress;
+
+    private Boolean defaultYN;
+
+    public DeliveryAddressDto toDTO(){
+        return DeliveryAddressDto.builder()
+                .id(id)
+                .recipient(recipient)
+                .destination(destination)
+                .recipientNumber(recipientNumber)
+                .zipCode(zipCode)
+                .address(address)
+                .detailAddress(detailAddress)
+                .defaultYN(defaultYN)
+                .build();
+    }
+}
